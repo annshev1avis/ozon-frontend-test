f989eb1901766b08342b70b52a1d5b69c7739530
Ann Shevchenko <annshev1220@gmail.com>
2026-04-22 23:17:01 +0300

    feat: сделана структура проекта и верстка

---
+++ app.js
@@ -0,0 +1,5 @@
+const valueInput = document.getElementById('value-input');
+const animateInput = document.getElementById('animate-input');
+const hideInput = document.getElementById('hide-input');
+
+const progressContainer = document.getElementById('progress-container');
